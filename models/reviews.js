3729b072305340279687fd40290f7ca158ea0921
Ayaz <ayazhussainbs@gmail.com>
2020-01-22 11:55:14 +0500

    Index removed from review schema

--- models/reviews.js
+++ models/reviews.js
@@ -16,11 +16,11 @@ var reviewSchema = new Schema({
     date: { type: Date, default: Date.now }
 }, { versionKey: false });
 
-reviewSchema.index({
-    "user": 1,
-    "product": 1
-}, {
-    "unique": true
-});
+// reviewSchema.index({
+//     "user": 1,
+//     "product": 1
+// }, {
+//     "unique": true
+// });
 
 module.exports = mongoose.model('Reviews', reviewSchema);
